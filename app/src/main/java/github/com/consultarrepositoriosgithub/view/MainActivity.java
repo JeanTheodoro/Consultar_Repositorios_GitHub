e3330ace99e6dba7c4196df6ed955ec8b10a290e
JeanTheodoro <jean.theodorosilva@gmail.com>
2020-10-20 21:13:28 -0300

    Implementação do layout

--- app/src/main/java/github/com/consultarrepositoriosgithub/view/MainActivity.java
+++ app/src/main/java/github/com/consultarrepositoriosgithub/view/MainActivity.java
@@ -1,8 +1,10 @@
-package github.com.consultarrepositoriosgithub;
+package github.com.consultarrepositoriosgithub.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import github.com.consultarrepositoriosgithub.R;
 
 public class MainActivity extends AppCompatActivity {
 
